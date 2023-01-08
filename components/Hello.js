72711cba0cb3e265f4205d4d4feeff7b8ea31a66
Julie Evans <juliemevans17@gmail.com>
2023-01-07 22:47:42 -0600

    Update hello button to mui fab button

--- components/Hello.js
+++ components/Hello.js
@@ -1,11 +1,16 @@
 import styles from '../styles/Home.module.css'
+import { Fab } from '@mui/material'
+import MailIcon from '@mui/icons-material/Mail'
 
 export default function Hello() {
   return (
     <div className={styles.section}>
       <h1>Hi, I'm Julie M. Evans</h1>
       <p>Web Developer / Software Engineer / Full-Stack Engineer</p>
-      <button>Contact Me</button>
+      <Fab variant="extended" color="primary">
+        <MailIcon sx={{ mr: 1 }} />
+        Contact Me
+      </Fab>
     </div>
   )
 }
