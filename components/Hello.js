f44c1d2f859f00f4e9220ba162d126d8eeb98e61
Julie Evans <juliemevans17@gmail.com>
2023-01-07 22:36:08 -0600

    Add hello component

---
+++ components/Hello.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Hello() {
+  return (
+    <div className={styles.section}>
+      <h1>Hi, I'm Julie M. Evans</h1>
+      <p>Web Developer / Software Engineer / Full-Stack Engineer</p>
+      <button>Contact Me</button>
+    </div>
+  )
+}
--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Hello from './Hello'
 import AboutMe from './AboutMe'
 import Projects from '../components/Projects'
 import Blogs from './Blogs'
@@ -11,11 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.section}>
-        <h1>Hi, I'm Julie M. Evans</h1>
-        <p>Web Developer / Software Engineer / Full-Stack Engineer</p>
-        <button>Contact Me</button>
-      </div>
+      <Hello />
 
       <AboutMe />
 
