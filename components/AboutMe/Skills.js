8d58e974c6b3e3e147807a0a6e493c0b932aa6d5
Julie Evans <juliemevans17@gmail.com>
2023-01-14 22:08:37 -0600

    update skills to scroll

--- components/AboutMe/Skills.js
+++ components/AboutMe/Skills.js
@@ -7,86 +7,35 @@ export default function Skills() {
 
   return (
     <>
-      <h2>Skills</h2>
+      {/* <h2>Skills</h2> */}
+      <Typography variant="h4">Skills</Typography>
+      <Divider width="100%" sx={{ margin: "0.5rem 0" }} />
 
-      {/* <Box
-        // className={styles.grid}
-        sx={{
-          // bgcolor: "background.paper", 
-          borderRadius: "12px"
-        }}
-      >
-        {skills[labels[0]].map((skill) => (
-          <Box 
-            key={skill.id} 
-            className={styles.card}
-            sx={{
-              display: "flex",
-              // justifyContent: "space-between",
-              alignItems: "center"
-            }}
-          >
-            <Avatar sx={{ mr: 1 }}>{skill.icon}</Avatar>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                alignItems: "stretch",
-                flexGrow: 1
-              }}
-            >
-              <Typography variant="h6">{skill.primary}</Typography>
-              <Typography variant="subtitle1">{skill.secondary}</Typography>
-            </Box>
+      <Box className={styles.scroll} sx={{ width: "100%" }}>
+        {labels.map((label) => (
+          <Box sx={{ margin: "1rem 0" }}>
+            <Typography variant="h6">
+              {label.charAt(0).toUpperCase() + label.slice(1)}
+            </Typography>
+
+            <List dense>
+              {skills[label].map((skill) => (
+                <ListItem key={skill.id}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      {skill.icon}
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText 
+                    primary={skill.primary} 
+                    secondary={skill.secondary} 
+                  />
+                </ListItem>
+              ))}
+            </List>
           </Box>
         ))}
-      </Box> */}
-
-      <List 
-        // className={styles.list}
-        className={styles.grid}
-        dense
-        sx={{
-          bgcolor: "background.paper", 
-          borderRadius: "12px",
-          // display: "grid",
-          // gridTemplateColumns: "repeat(3, 200px)",
-          // width: "100%"
-        }}
-      >
-        {labels.map((label) => (
-          <>
-            <ListItem sx={{ gridColumn: "1 / -1", textAlign: "center" }}>
-                <ListItemText primary={
-                  <Typography variant="h6">
-                    {label.charAt(0).toUpperCase() + label.slice(1)}
-                  </Typography>
-                } />
-            </ListItem>
-
-            {skills[label].map((skill) => (
-              <ListItem key={skill.id}>
-                <ListItemAvatar>
-                  <Avatar>
-                    {skill.icon}
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText 
-                  primary={skill.primary} 
-                  secondary={skill.secondary} 
-                />
-              </ListItem>
-            ))}
-
-            {labels.indexOf(label) === labels.length-1 ? (
-              <></>
-            ) : (
-              <Divider sx={{ gridColumn: "1 / -1", mt: "8px" }} />
-            )}
-          </>
-        ))}
-      </List>
+      </Box>
     </>
   )
 }
