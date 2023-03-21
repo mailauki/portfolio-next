89e4fca3825765143ccb3c69548adac3dcd20eb0
Julie Evans <juliemevans17@gmail.com>
2023-03-21 18:47:10 -0500

    Update skills list styles

--- components/AboutMe/Skills.js
+++ components/AboutMe/Skills.js
@@ -1,38 +1,28 @@
 import styles from '../../styles/Home.module.css'
-import { List, ListItem, ListItemAvatar, Avatar, ListItemText, Divider, Typography, Box } from '@mui/material'
+import { ListItem, ListItemAvatar, Avatar, ListItemText, Divider, Typography, Box, Stack } from '@mui/material'
 import { skills } from '../../data/skills'
 
 export default function Skills() {
-  const labels = Object.keys(skills)
-
   return (
     <>
       <Typography variant="h4">Skills</Typography>
       <Divider width="100%" sx={{ margin: "0.5rem 0" }} />
 
-      <Box className={styles.scroll} sx={{ width: "100%" }}>
-        {labels.map((label) => (
-          <Box sx={{ margin: "1rem 0" }} key={label}>
-            <Typography variant="h6">
-              {label.charAt(0).toUpperCase() + label.slice(1)}
-            </Typography>
-
-            <List dense>
-              {skills[label].map((skill) => (
-                <ListItem key={skill.id}>
-                  <ListItemAvatar>
-                    <Avatar>
-                      {skill.icon}
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText 
-                    primary={skill.primary} 
-                    secondary={skill.secondary} 
-                  />
-                </ListItem>
-              ))}
-            </List>
-          </Box>
+      <Box className={styles.list} sx={{ alignItems: "center", justifyContent: "center" }}>
+        {skills.map((skill) => (
+          <Stack direction="row" flexWrap="wrap" justifyContent="center">
+              <ListItem key={skill.id} sx={{ width: "200px" }}>
+                <ListItemAvatar>
+                  <Avatar>
+                    {skill.icon}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText 
+                  primary={skill.primary} 
+                  secondary={skill.secondary} 
+                />
+              </ListItem>
+          </Stack>
         ))}
       </Box>
     </>
--- data/skills.js
+++ data/skills.js
@@ -11,72 +11,61 @@ import NextJsIcon from '../icons/NextJsIcon'
 import PostgresIcon from '../icons/PostgresIcon'
 import TypeScriptIcon from '../icons/TypeScriptIcon'
 
-export const skills = {
-  languages: [
-    { 
-      id: "html",
-      primary: "HTML",
-      icon: <HtmlIcon />
-    },
-    { 
-      id: "css",
-      primary: "CSS",
-      icon: <CssIcon />
-    },
-    { 
-      id: "javascript",
-      primary: "JavaScript",
-      icon: <JavaScriptIcon />
-    },
-    { 
-      id: "ruby",
-      primary: "Ruby",
-      icon: <RubyIcon />
-    },
-    {
-      id: "sql",
-      primary: "SQL",
-      icon: <CodeIcon />
-    },
-    { 
-      id: "typescript",
-      primary: "TypeScript",
-      secondary: "Learning",
-      icon: <TypeScriptIcon />
-    }
-  ],
-  frameworks: [
-    { 
-      id: "reactjs",
-      primary: "React JS",
-      icon: <ReactIcon />
-    },
-    { 
-      id: "ruby-on-rails",
-      primary: "Ruby on Rails",
-      icon: <RailsIcon />
-    },
-    {
-      id: "active-record",
-      primary: "Active Record",
-      icon: <CodeIcon />
-    },
-    { 
-      id: "nextjs",
-      primary: "Next JS",
-      icon: <NextJsIcon />
-    }
-  ],
-  other: [
-    { 
-      id: "redux",
-      primary: "Redux",
-      icon: <ReduxIcon />
-    },
-    { 
-      id: "postgres",
-      primary: "Postgres",
-      icon: <PostgresIcon />
-    }
-  ]
-}
+export const skills = [
+  { 
+    id: "html",
+    primary: "HTML",
+    icon: <HtmlIcon />
+  },
+  { 
+    id: "css",
+    primary: "CSS",
+    icon: <CssIcon />
+  },
+  { 
+    id: "javascript",
+    primary: "JavaScript",
+    icon: <JavaScriptIcon />
+  },
+  { 
+    id: "ruby",
+    primary: "Ruby",
+    icon: <RubyIcon />
+  },
+  {
+    id: "sql",
+    primary: "SQL",
+    icon: <CodeIcon />
+  },
+  { 
+    id: "typescript",
+    primary: "TypeScript",
+    secondary: "Learning",
+    icon: <TypeScriptIcon />
+  },
+  { 
+    id: "reactjs",
+    primary: "React JS",
+    icon: <ReactIcon />
+  },
+  { 
+    id: "ruby-on-rails",
+    primary: "Ruby on Rails",
+    icon: <RailsIcon />
+  },
+  { 
+    id: "nextjs",
+    primary: "Next JS",
+    icon: <NextJsIcon />
+  },
+  { 
+    id: "redux",
+    primary: "Redux",
+    icon: <ReduxIcon />
+  },
+  { 
+    id: "postgres",
+    primary: "PostgreSQL",
+    icon: <PostgresIcon />
+  }
+]
